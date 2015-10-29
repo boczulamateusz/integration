8ff6fba123c74f847f0ad0571bcad518661f17a4
Mateusz Boczula <kareem.jabaar@gmail.com>
2015-10-29 21:32:52 +0100

    final changes p2

--- src/main/java/boczula/mateusz/integration/App.java
+++ src/main/java/boczula/mateusz/integration/App.java
@@ -2,10 +2,6 @@ package boczula.mateusz.integration;
 
 import com.flexionmobile.codingchallenge.integration.IntegrationTestRunner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
